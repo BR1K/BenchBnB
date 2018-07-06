d25813f7fe1e37eec681a9f672c1a75fd26fb724
Brikend <brikendbehrami@gmail.com>
2018-07-06 17:37:03 -0400

    end of day commit

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -4,15 +4,16 @@ import * as SessionAPIUtil from './util/session_api_util';
 import { merge } from 'lodash';
 import configureStore from './store/store';
 import {login, logout, signup} from './actions/session_actions.js';
-
+import Root from "./components/root"
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 	const store = configureStore();
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
+
 	window.getState = store.getState;
 	window.dispatch = store.dispatch;
-
 	window.login = login;
 	window.logout = logout;
 	window.signup = signup;
