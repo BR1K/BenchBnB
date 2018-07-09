9993c5c1a72921429208d205b640dd4e80ae2f06
Brikend <brikendbehrami@gmail.com>
2018-07-09 13:35:57 -0400

    add render function

--- BenchBnB/frontend/components/greeting/greeting.jsx
+++ BenchBnB/frontend/components/greeting/greeting.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Greeting extends React.Component{
-	
+  render() {
+
+    if (this.props.currentUser) {
+      return (
+        <div>
+          <h1>Welcome, {this.props.currentUser.username}</h1>
+          <button onClick={this.props.logout}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/api/login">Login</Link>
+          <Link to="/api/signup">Signup</Link>
+        </div>
+      );
+    }
+  }
+
 }
 
 export default Greeting;
