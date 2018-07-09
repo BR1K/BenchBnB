bad55145ed017e060a364b28a40572174ed42030
Brikend <brikendbehrami@gmail.com>
2018-07-09 15:35:58 -0400

    change RECEIVE_ERRORS to RECEIVE_SESSION_ERRORS

--- BenchBnB/frontend/actions/session_actions.js
+++ BenchBnB/frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as SessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = (user) => {
 	return {
@@ -17,21 +17,27 @@ export const logoutCurrentUser = () => {
 	};
 };
 
-export const receiveErrors = (errors) => {
+export const receiveSessionErrors = (errors) => {
 	return {
-		type: RECEIVE_ERRORS,
-		errors
+		type: RECEIVE_SESSION_ERRORS,
+		errors: errors.responseJSON
 	};
 };
 
 export const login = user => dipatch => {
-	SessionAPIUtil.login(user).then( (user) => dispatch(receiveCurrentUser(user)));
+	return SessionAPIUtil.login(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveSessionErrors(errors))
+  );
 };
 
 export const logout = () => dispatch => {
-	SessionAPIUtil.logout().then( (user) => dispatch(logoutCurrentUser()));
+	return SessionAPIUtil.logout().then((user) => dispatch(logoutCurrentUser()));
 };
 
 export const signup = user => dispatch => {
-	SessionAPIUtil.signup(user).then( (user) => dispatch(receiveCurrentUser(user)));
+	return SessionAPIUtil.signup(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveSessionErrors(errors))
+  );
 };
