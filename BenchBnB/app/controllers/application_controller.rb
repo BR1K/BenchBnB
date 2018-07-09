7ac33e55150224ed55786828eb8bb7a23a6097c0
Brikend <brikendbehrami@gmail.com>
2018-07-09 15:38:02 -0400

    add helper methods

--- BenchBnB/app/controllers/application_controller.rb
+++ BenchBnB/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :current_user
+  helper_method :logged_in?
+
 	def current_user
 		@current_user ||= User.find_by(session_token: session[:session_token])
 	end
